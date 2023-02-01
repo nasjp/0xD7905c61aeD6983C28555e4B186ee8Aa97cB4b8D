f054321f41021748d2c906d69a0965de31740ea2
nas <y.softvalley@hey.com>
2023-02-01 20:36:10 +0900

    add favicon

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge: false,
+			external: [],
+			split: false
+		})
 	}
 };
-
-export default config;
